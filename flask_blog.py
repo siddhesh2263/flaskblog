0dbcc236ba99913fe782fc154d8b350946a90451
siddhesh2263 <siddhesh2263@gmail.com>
2020-12-17 10:56:45 +0530

    Added templates

--- flask_blog.py
+++ flask_blog.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Sid Marker',
+        'title': 'Only Time',
+        'content': 'Time Will Tell',
+        'date_posted': '24 Feb'
+    },
+    {
+        'author': 'John',
+        'title': 'Enemies',
+        'content': 'Time Will Tell All',
+        'date_posted': '23 Feb'
+    }
+]
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('about.html', title="About")
 
 if __name__ == '__main__':
     app.run(debug=True)
